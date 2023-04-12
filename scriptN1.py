e63fa6030e86d8e81fbaa4cd84307444ffcb4a0f
jackcooper <alonsso008@gmail.com>
2023-04-12 11:34:44 -0400

    Python3 Scripts Added

---
+++ scriptN1.py
@@ -0,0 +1,11 @@
+
+al1 = "Matias Toro " 
+al2 = "Alonso Guajardo "
+al3 = "Benjamin Sepulveda "
+
+
+print("Evaluación N°1 Programación y Redes Virtualizadas")
+print("Alumnos:")
+print(al1)
+print(al2)
+print(al3)
---
+++ scriptN2.py
@@ -0,0 +1,12 @@
+#nombre
+nombre = input("Ingrese su nombre: ")
+#apellido
+apellido = input("Ingrese su apellido: ")
+#codigo_seccion
+code_sec = input("Codigo seccion: ")
+#sede
+sede = input("Ingresa tu sede: ")
+
+space = " "
+
+print(f"\n Hola! {nombre+space+apellido } \n Tu sede es: {sede} \n El codigo de tu seccion es: {code_sec}")
