b31e62c32d0b61b620256abd8940e0164b21bb83
jackcooper <jackcooper@BT-7275>
2023-07-04 23:13:01 -0400

    Anadido el archivo del item numero 1

---
+++ Archivos_Examen_Transversal/item1.py
@@ -0,0 +1,14 @@
+import os
+
+os.system('clear')
+
+integrantes = ['Alonso Guajardo', 'Matias Toro', 'Benjamin Sepulveda']
+
+print('Nombre de Asignatura: PROGRAMACION Y REDES VIRTUALIZADAS')
+print('ID de Asignatura: DRY7122')
+print('Seccion: 001D')
+print('Docente: Horacio Amador Vega Fuentes \n')
+
+print('Integrantes:')
+for integrante in integrantes:
+    print(f'-> {integrante}')
